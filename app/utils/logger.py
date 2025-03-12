3315fdc908d626b90b1c1fabd615758d8cbdd958
Bohdan Rak <bodua55@gmail.com>
2025-03-13 00:53:19 +0200

    Change logging to file

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 
 def log_to_console(symbol, message):
@@ -5,7 +6,8 @@ def log_to_console(symbol, message):
     current_time = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
     print(f"{symbol} {current_time} {message}")
 
-def log_to_file(message, filename="log.txt"):
+def log_to_file(message, filename="logs/log.txt"):
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     with open(filename, "a", encoding="utf-8") as log_file:
         log_file.write(f"{current_time} {message}\n")
